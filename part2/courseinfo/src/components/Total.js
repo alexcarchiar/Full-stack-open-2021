1061924f35f9ef9728a2ce66705a14ee526d4cfe
Chiarelli Alessandro <a.chiarelli@reply.it>
2021-09-09 18:35:42 +0300

    Update Total.js
    up until 2.3 done

--- part2/courseinfo/src/components/Total.js
+++ part2/courseinfo/src/components/Total.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
 const Total = ({ course }) => {
-    let sum = 0;
-    for (let i = 0; i<course.parts.length; i++){
-        sum += course.parts[i].exercises
-    }
+    
+    const sum = course.parts.reduce( (previous ,part) => {
+        return previous + part.exercises
+    }, 0)
     return(
       <b>Number of exercises {sum}</b>
     ) 
