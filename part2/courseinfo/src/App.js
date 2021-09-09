2175bd4939029e2be2fa41c26629e32bef670d15
Chiarelli Alessandro <a.chiarelli@reply.it>
2021-09-09 18:43:27 +0300

    Completed up until 2.5

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -11,7 +11,7 @@ const Total = ({ course }) => {
 
 const App = () => {
 
-  const course = {
+  /*const course = {
     name: 'Half Stack application development',
     parts: [
       {
@@ -31,9 +31,62 @@ const App = () => {
         exercises: 11
       }
     ]
-  }
+  }*/
 
-  return <Course course={course} />
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
+
+  return (
+    <div>
+      <h1>Web development curriculum</h1>
+      {courses.map( 
+              course =>
+               <Course key={course.id}course={course} />
+            )}
+    </div>
+    )
 }
 
 export default App;
