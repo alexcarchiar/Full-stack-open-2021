dd890bc5e8e0c186367ad5765c7cd3ff4db6fd7e
Chiarelli Alessandro <a.chiarelli@reply.it>
2021-09-11 19:48:52 +0300

    Update App.js

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import Filter from './components/Filter'
 import Form from './components/Form'
 import Persons from './components/Persons'
+import axios from 'axios'
 
 const App = () =>  {
-  const [ persons, setPersons ] = useState([
+  /*const [ persons, setPersons ] = useState([
     { name: 'Arto Hellas',
       number: '040-1234567'
    },
@@ -14,8 +15,19 @@ const App = () =>  {
     number: '39-44-5323523'
   },
   { name: 'Dan Abramov', number: '12-43-234345' },
-  { name: 'Mary Poppendieck', number: '39-23-6423122'}
-  ]) 
+  { name: 'Mary Poppendieck', number: '39-23-6423122'},
+  { name: 'Mario Rossi', number: '39-23-7445551'}
+  ]) */
+  const [persons, setPersons ] = useState([])
+
+  useEffect( () => {
+    axios
+    .get('http://localhost:3001/persons')
+    .then( r => {
+      setPersons(r.data)
+    })
+  }, [])
+
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber] = useState('')
   const [ newFilter, setFilter] = useState('')
