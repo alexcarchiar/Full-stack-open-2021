bec021528109023eeac1957d762742d49e922634
Chiarelli Alessandro <a.chiarelli@reply.it>
2021-09-11 17:53:23 +0300

    Update App.js

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,24 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react'
 
 const App = () =>  {
   const [ persons, setPersons ] = useState([
     { name: 'Arto Hellas' }
   ]) 
   const [ newName, setNewName ] = useState('')
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    console.log('button clicked', event.target)
+    const person = {
+      name: newName
+    }
+    setPersons(persons.concat(person))
+    setNewName('')
+  }
+
+  const handleNameChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addPerson}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleNameChange}/>
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      
+        <div id='numbers'>
+          <ul>
+            {persons.map( p => 
+              <li key={p.name}>{p.name}</li>)}
+          </ul>
+        </div>
     </div>
   );
 }
