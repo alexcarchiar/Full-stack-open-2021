89797de588df9310ca6f5373ca2be55127ab819e
Chiarelli Alessandro <a.chiarelli@reply.it>
2021-09-11 18:21:14 +0300

    Update App.js

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -4,9 +4,17 @@ import React, {useState} from 'react'
 
 const App = () =>  {
   const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas',
+      number: '040-1234567'
+   },
+   { name: 'Ada Lovelace',
+    number: '39-44-5323523'
+  },
+  { name: 'Dan Abramov', number: '12-43-234345' },
+  { name: 'Mary Poppendieck', number: '39-23-6423122'}
   ]) 
   const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber] = useState('')
 
   
 
@@ -14,7 +22,8 @@ const App = () =>  {
     event.preventDefault()
     console.log('button clicked', event.target)
     const person = {
-      name: newName
+      name: newName,
+      number: newNumber
     }
     if(persons.filter(p => p.name === person.name).length == 0){ //checking that the person is already included
       //apparently, JS is like Java or Python: if you put objects in an array, you need to manually compare the fields
@@ -23,11 +32,17 @@ const App = () =>  {
       alert(newName + ' is already added')
     }
     setNewName('')
+    setNewNumber('')
   }
 
   const handleNameChange = (event) => {
     console.log(event.target.value)
     setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    console.log(event.target.value)
+    setNewNumber(event.target.value)
   }
 
   return (
@@ -36,6 +51,8 @@ const App = () =>  {
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handleNameChange}/>
+          <br/>
+          number: <input value={newNumber} onChange={handleNumberChange} />
         </div>
         <div>
           <button type="submit">add</button>
@@ -45,7 +62,7 @@ const App = () =>  {
         <div id='numbers'>
           <ul>
             {persons.map( p => 
-              <li key={p.name}>{p.name}</li>)}
+              <li key={p.name}>{p.name} {p.number}</li>)}
           </ul>
         </div>
     </div>
