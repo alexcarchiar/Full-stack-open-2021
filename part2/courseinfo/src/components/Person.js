744505eeecab9c180120c52309e8755dff0baaea
Chiarelli Alessandro <a.chiarelli@reply.it>
2021-09-11 18:45:58 +0300

    added person module

---
+++ part2/courseinfo/src/components/Person.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Person = (props) => {
+    return(
+        <li key={props.name}>{props.name} {props.number}</li>
+    )
+}
+
+export default Person
--- part2/phonebook/src/components/Persons.js
+++ part2/phonebook/src/components/Persons.js
@@ -1,11 +1,13 @@
 import React from 'react'
+import Person from './Person'
 
 const Persons = (props) => {
     return(
         <div id='numbers'>
           <ul>
             {props.persons.filter(p => p.name.toLowerCase().match(props.newFilter.toLowerCase())).map( p => 
-              <li key={p.name}>{p.name} {p.number}</li>)}
+              //<li key={p.name}>{p.name} {p.number}</li>)}
+              <Person name={p.name} number={p.number}/>)}
           </ul>
         </div>
     )
