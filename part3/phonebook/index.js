108ced4f4f88a4275dcad7cec457069472f15373
Chiarelli Alessandro <a.chiarelli@reply.it>
2021-09-13 18:50:52 +0300

    up until 3.2 completed

---
+++ part3/phonebook/index.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const app = express()
+
+let phonebook = [
+    { 
+      "id": 1,
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": 2,
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": 3,
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": 4,
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+app.get('/', (request, response) => {
+    response.send('<h1>Hello World!</h1><a href="/api/phonebook">click here for phonebook api</a>')
+  })
+  
+  app.get('/api/phonebook', (request, response) => {
+    response.json(phonebook)
+  })
+
+  app.get('/info', (request, response) => {
+      const numPeople = phonebook.length
+      let htmlString = '<p>Phonebook has info for ' + numPeople + ' people</p><p>' + new Date() + '</p>'
+      response.send(htmlString)
+  })
+  
+  const PORT = 3001
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+  })
