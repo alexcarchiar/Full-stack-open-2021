4dc3669950042392bf88c8a30dc68c64836ccff1
Chiarelli Alessandro <a.chiarelli@reply.it>
2021-09-15 23:16:00 +0300

    completed 3.8

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -2,7 +2,17 @@ const express = require('express')
 const morgan = require('morgan')
 const app = express()
 app.use(express.json())
-app.use(morgan('combined'))
+//app.use(morgan('combined'))
+morgan.token('post', (request) => {
+  if (request.method === 'POST')
+    return JSON.stringify(request.body)
+  else
+    return ''
+})
+morgan.format('postFormat', ':method :url :status :res[content-length] - :response-time ms :post')
+app.use(morgan('postFormat'))
+
+
 
 let phonebook = [
     { 
