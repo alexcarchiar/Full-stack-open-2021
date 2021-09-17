5a2f875296d6c87b1cdf738e8442cb117a8e7e9b
Chiarelli Alessandro <a.chiarelli@reply.it>
2021-09-17 23:51:35 +0300

    up until 3.14 complete

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,9 +1,9 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
 app.use(express.json())
 app.use(express.static('build'))
-//app.use(morgan('combined'))
 morgan.token('post', (request) => {
   if (request.method === 'POST')
     return JSON.stringify(request.body)
@@ -15,6 +15,12 @@ app.use(morgan('postFormat'))
 const cors = require('cors')
 app.use(cors())
 
+const mongoose = require('mongoose')
+const Person = require('./models/person')
+const password = "fullstack"
+const url =
+  `mongodb+srv://fullstack:${password}@cluster0.zojao.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+  mongoose.connect(url)
 
 
 let phonebook = [
@@ -42,70 +48,68 @@ let phonebook = [
 
 app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1><a href="/api/phonebook">click here for phonebook api</a>')
-  })
+})
   
-  app.get('/api/phonebook', (request, response) => {
+app.get('/api/phonebook', (request, response) => {
+  Person.find({}).then( people => {
+    phonebook = people
     response.json(phonebook)
-
   })
+})
 
-  app.get('/api/phonebook/:id', (request, response) => {
-      const id = Number(request.params.id)
-      const person = phonebook.find(e => e.id === id)
-      if(!person){
-          response.status(404).end
-      }
-      response.json(person)
+app.get('/api/phonebook/:id', (request, response) => {
+  Person.findById(request.params.id).then(note => {
+    response.json(note)
   })
+})
 
-  app.get('/info', (request, response) => {
-      const numPeople = phonebook.length
-      let htmlString = '<p>Phonebook has info for ' + numPeople + ' people</p><p>' + new Date() + '</p>'
-      response.send(htmlString)
-  })
+app.get('/info', (request, response) => {
+    let numPeople = phonebook.length
+    Person.find({}).then( people => {
+      phonebook = people
+    })
+    let htmlString = '<p>Phonebook has info for ' + numPeople + ' people</p><p>' + new Date() + '</p>'
+    response.send(htmlString)
+})
 
-  app.delete('/api/phonebook/:id', (request, response) => {
-    const id = Number(request.params.id)
-    phonebook = phonebook.filter(e => e.id !== id)
-    response.status(204).end()
-  })
+app.delete('/api/phonebook/:id', (request, response) => {
+  const id = Number(request.params.id)
+  phonebook = phonebook.filter(e => e.id !== id)
+  response.status(204).end()
+})
+
+app.put('/api/phonebook/:id', (request, response) => {
+  const id = Number(request.params.id)
+  let person = phonebook.find(e => e.id === id)
+  person.number = request.body.number
+  response.json(person)
+  response.status(400)
+})
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
   
-  app.put('/api/phonebook/:id', (request, response) => {
-    const id = Number(request.params.id)
-    let person = phonebook.find(e => e.id === id)
-    person.number = request.body.number
-    response.json(person)
+  if(!body.name){
+      response.status(400)
+      return response.json({error: 'need a name'}).end()
+  } else if(!body.number){
     response.status(400)
+    return response.json({error: 'need a number'}).end()
+  } else if(phonebook.find(e => e.name === body.name)){
+    response.status(400)
+    return response.json({error: 'need a unique name'}).end()
+}
+  const person = new Person({
+      name: body.name,
+      number: body.number
   })
 
-  app.post('/api/persons', (request, response) => {
-    let newId = Math.floor(Math.random() * 10000)
-      while(phonebook.find(e => e.id === newId)){
-        newId = Math.floor(Math.random() * 10000)
-      }
-
-      const body = request.body
-      if(!body.name){
-          response.status(400)
-          return response.json({error: 'need a name'}).end()
-      } else if(!body.number){
-        response.status(400)
-        return response.json({error: 'need a number'}).end()
-      } else if(phonebook.find(e => e.name === body.name)){
-        response.status(400)
-        return response.json({error: 'need a unique name'}).end()
-    }
-      const person = {
-          id: newId,
-          name: body.name,
-          number: body.number
-      }
-
-      phonebook = phonebook.concat(person)
-      response.json(person)
+  person.save().then(savedPerson => {
+    response.json(savedPerson)
   })
+})
 
-  const PORT = process.env.PORT || 3001
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-  })
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
