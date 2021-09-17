4f870ab77a5223a11e2734b332ede8dbe94f6b49
Chiarelli Alessandro <a.chiarelli@reply.it>
2021-09-17 22:57:31 +0300

    Update mongo.js

--- part3/phonebook/mongo.js
+++ part3/phonebook/mongo.js
@@ -1,35 +1,3 @@
-/*const mongoose = require('mongoose')
-
-if (process.argv.length < 3) {
-  console.log('Please provide the password as an argument: node mongo.js <password>')
-  process.exit(1)
-}
-
-const password = process.argv[2]
-
-const url =
-  `mongodb+srv://fullstack:${password}@cluster0.zojao.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
-
-mongoose.connect(url)
-
-const personSchema = new mongoose.Schema({
-    name: String,
-    number: String,
-})
-
-const Person = mongoose.model('Person', personSchema)
-
-const person = new Person({
-    name: "Ada Lovelace",
-    number: "040-1231236",
-  })
-  
-  person.save().then(result => {
-    console.log('person saved!')
-    mongoose.connection.close()
-  })
-*/
-
 const mongoose = require('mongoose')
 
 if (process.argv.length < 3) {
