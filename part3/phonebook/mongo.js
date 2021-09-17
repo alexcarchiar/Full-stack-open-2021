df99247ba47fa42c7d36437c23530f27c4fbda13
Chiarelli Alessandro <a.chiarelli@reply.it>
2021-09-17 22:55:07 +0300

    completed 3.12

---
+++ part3/phonebook/mongo.js
@@ -0,0 +1,85 @@
+/*const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log('Please provide the password as an argument: node mongo.js <password>')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url =
+  `mongodb+srv://fullstack:${password}@cluster0.zojao.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+const person = new Person({
+    name: "Ada Lovelace",
+    number: "040-1231236",
+  })
+  
+  person.save().then(result => {
+    console.log('person saved!')
+    mongoose.connection.close()
+  })
+*/
+
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log('Please provide the password as an argument: node mongo.js <password>')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url =
+  `mongodb+srv://fullstack:${password}@cluster0.zojao.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if(process.argv.length === 3){
+    console.log("woo")
+    Person.find({}).then(result => {
+        result.forEach(person => {
+        console.log(person)
+        })
+        mongoose.connection.close()
+    })
+    process.exit(0)
+}
+
+if(process.argv.length === 4){
+    console.log('Please provide the phone number as an argument')
+    process.exit(1)
+}
+
+if(process.argv.length === 5){
+    const person = new Person({
+        name: process.argv[3],
+        number: process.argv[4],
+      })
+      
+      person.save().then(result => {
+        console.log('person saved!')
+        mongoose.connection.close()
+      })
+}
+
+if(process.argv.length > 5){
+    console.log('Please check the number of arguments')
+    process.exit(1)
+}
