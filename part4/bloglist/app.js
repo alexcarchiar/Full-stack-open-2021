0f0cc6d2e02c7c28573db4b1ba1152cd3db6bf0b
Chiarelli Alessandro <a.chiarelli@reply.it>
2021-09-19 00:57:02 +0300

    4.2 completed

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -4,6 +4,7 @@ const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
 const config = require('./utils/config')
+const blogsRouter = require('./controllers/blogs')
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 
@@ -19,8 +20,8 @@ app.use(middleware.requestLogger)
 app.get('/', (request, response) => {
     response.send('<h1>Hello world</h1>')
   })
-
-app.get('/api/blogs', (request, response) => {
+app.use('/api/blogs', blogsRouter)
+/*app.get('/api/blogs', (request, response) => {
   Blog
     .find({})
     .then(blogs => {
@@ -36,7 +37,7 @@ app.post('/api/blogs', (request, response) => {
     .then(result => {
       response.status(201).json(result)
     })
-})
+})*/
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
