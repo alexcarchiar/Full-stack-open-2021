dcd04a3f395162e5c04b305005cd2b05e7dffebb
Chiarelli Alessandro <a.chiarelli@reply.it>
2021-09-19 11:32:18 +0300

    4.4 done

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,7 +1,18 @@
+const blog = require('../models/blog')
+const Blog = require('../models/blog')
 const dummy = (blogs) => {
     return 1
 }
 
+const totalLikes = (blogs) => {
+    let sum = 0
+    blogs.forEach(element => {
+        sum = sum + element.likes
+    });
+    return sum
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
