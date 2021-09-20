d3e2ca0f6f4fd39da1bfce1777943a25e7519eca
Chiarelli Alessandro <a.chiarelli@reply.it>
2021-09-20 20:29:47 +0300

    boh

--- part4/bloglist/tests/likes.test.js
+++ part4/bloglist/tests/likes.test.js
@@ -68,13 +68,6 @@ describe('total likes', () => {
             "id": "6146f8483ce6a17d5f6c9689"
           }
       ]
-      const favorite = {
-        "title": "Apple",
-        "author": "Steve Jobs",
-        "url": "apple.com",
-        "likes": 100,
-        "id": "6146607f8d295c0be577a26e"
-      }
   
     test('when list has only one blog, equals the likes of that', () => {
       const result = listHelper.favoriteBlog(listWithOneBlog)
@@ -84,5 +77,149 @@ describe('total likes', () => {
     test('checking with long list', () => {
         const result = listHelper.favoriteBlog(listWithManyBlogs)
         expect(result).toBe(listWithManyBlogs[3])
+      })
+  })
+
+  describe('mostBlogs', () => {
+    const listWithOneBlog = [
+      {
+        _id: '5a422aa71b54a676234d17f8',
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+        likes: 5,
+        __v: 0
+      }
+    ]
+  
+    const listWithManyBlogs = [
+        {
+          _id: '5a422aa71b54a676234d17f8',
+          title: 'Go To Statement Considered Harmful',
+          author: 'Edsger W. Dijkstra',
+          url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+          likes: 5,
+          __v: 0
+        },
+        {
+            "title": "Alessandro Chiarelli",
+            "author": "alexcarchiar",
+            "url": "alessandrochiarelli.com",
+            "likes": 10,
+            "id": "614642944c2d68b8ac8dd311"
+          },
+          {
+            "title": "Lorenzo Callegari",
+            "author": "lorcalhost",
+            "url": "lorcalhost.com",
+            "likes": 10,
+            "id": "61465e591293c958022e01f4"
+          },
+          {
+            "title": "Apple",
+            "author": "Steve Jobs",
+            "url": "apple.com",
+            "likes": 100,
+            "id": "6146607f8d295c0be577a26e"
+          },
+          {
+            "title": "Canonical string reduction",
+            "author": "alexcarchiar",
+            "url": "dijkstra.com",
+            "likes": 12,
+            "id": "6146f8483ce6a17d5f6c9689"
+          }
+      ]
+
+      const result1 = {
+          author: "Edsger W. Dijkstra",
+          blogs: 1
+      }
+
+      const result2 = {
+          author: "alexcarchiar",
+          blogs: 2
+      }
+
+    test('when list has only one blog, equals the likes of that', () => {
+      const result = listHelper.mostBlogs(listWithOneBlog)
+      expect(result).toBe(result1)
+    })
+
+    test('long list', () => {
+        const result = listHelper.mostBlogs(listWithManyBlogs)
+        expect(result).toBe(result2)
+      })
+  })
+
+  describe('most likes', () => {
+    const listWithOneBlog = [
+      {
+        _id: '5a422aa71b54a676234d17f8',
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+        likes: 5,
+        __v: 0
+      }
+    ]
+  
+    const listWithManyBlogs = [
+        {
+          _id: '5a422aa71b54a676234d17f8',
+          title: 'Go To Statement Considered Harmful',
+          author: 'Edsger W. Dijkstra',
+          url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+          likes: 5,
+          __v: 0
+        },
+        {
+            "title": "Alessandro Chiarelli",
+            "author": "alexcarchiar",
+            "url": "alessandrochiarelli.com",
+            "likes": 10,
+            "id": "614642944c2d68b8ac8dd311"
+          },
+          {
+            "title": "Lorenzo Callegari",
+            "author": "lorcalhost",
+            "url": "lorcalhost.com",
+            "likes": 10,
+            "id": "61465e591293c958022e01f4"
+          },
+          {
+            "title": "Apple",
+            "author": "Steve Jobs",
+            "url": "apple.com",
+            "likes": 100,
+            "id": "6146607f8d295c0be577a26e"
+          },
+          {
+            "title": "Canonical string reduction",
+            "author": "alexcarchiar",
+            "url": "dijkstra.com",
+            "likes": 12,
+            "id": "6146f8483ce6a17d5f6c9689"
+          }
+      ]
+
+      const result1 = {
+          author: "Edsger W. Dijkstra",
+          likes: 5
+      }
+
+      const result2 = {
+        author: "Steve Jobs",
+        likes: 100
+      }
+
+    test('when list has only one blog, equals the likes of that', () => {
+      const result = listHelper.mostLikes(listWithOneBlog)
+      expect(result).toBe(result1)
+    })
+
+    test('long list', () => {
+        const result = listHelper.mostLikes(listWithOneBlog)
+        expect(result).toBe(result2)
       })
   })
--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,5 +1,6 @@
 const blog = require('../models/blog')
 const Blog = require('../models/blog')
+const lodash = require('lodash')
 const dummy = (blogs) => {
     return 1
 }
@@ -19,11 +20,54 @@ const favoriteBlog = (blogs) => {
             favorite = el
         }
     })
-    return favorite;
+    return favorite
 }
+
+const mostBlogs = (blogs) => {
+    if (blogs.length === 0) {
+      return null
+    }
+
+    let authorsAndBlogs = blogs.countBy("author");
+    const sortedBlogs = Object.values(authorsAndBlogs).sort((a, b) => b - a);
+    const AuthorsByBlog = Object.keys(authorsAndBlogs).sort(
+      (a, b) => authorsAndBlogs[b] - authorsAndBlogs[a]
+    )
+
+    const mostBlogsObject = { author: AuthorsByBlog[0], blogs: sortedBlogs[0] };
+    return mostBlogsObject
+  };
+
+  const mostLikes = (blogs) => {
+    if (blogs.length === 0) {
+      return null
+    }
+  
+    const reducer = (accumulator, blog) => {
+        if(accumulator[blog.author] === undefined){
+            return { ...accumulator, [blog.author]: blog.likes }
+        } else {
+           return { ...accumulator, [blog.author]: accumulator[blog.author] + blog.likes }
+        }
+    }
+  
+    const likes = blogs.reduce(reducer, {});
+    const mostLikedAuthor = Object.keys(likes).sort(
+      (a, b) => likes[b] - likes[a]
+    )[0];
+  
+    const mostLikesObject = {
+      author: mostLikedAuthor,
+      likes: likes[mostLikedAuthor],
+    };
+  
+    return mostLikesObject;
+  };
 
 module.exports = {
     dummy,
     totalLikes,
-    favoriteBlog
+    favoriteBlog,
+    mostBlogs,
+    mostLikes
 }
