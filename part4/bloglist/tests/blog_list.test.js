9e70d21d8f298962e996c33c14ca88287b873237
Chiarelli Alessandro <a.chiarelli@reply.it>
2021-09-24 23:55:36 +0300

    4.10 completed

--- part4/bloglist/tests/blog_list.test.js
+++ part4/bloglist/tests/blog_list.test.js
@@ -2,7 +2,7 @@ const { TestWatcher } = require('@jest/core')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
-
+const Blog = require('../models/blog')
 const api = supertest(app)
 
 test('Testing /api/blogs', async () => {
@@ -16,3 +16,17 @@ test('Testing that the blog posts have unique ids', async () => {
     expect(response.body[0]._id).not.toBeDefined()
 })
 
+test('Testing post /api/blogs', async () => {
+    let response = await api.get('/api/blogs')
+    const initialNumber = response.body.length
+    expect(initialNumber).toBe(4)
+    const newBlog = new Blog({
+        title: "Bitcoin",
+        author: "alexcarchiar",
+        url: "bitcoin.org",
+        likes: 10
+    })
+    await newBlog.save()
+    response = await api.get('/api/blogs')
+    expect(response.body).toHaveLength(initialNumber+1)
+})
